04c62ed7ec057b8be8c8d015dd6ccc69150b0367
Andreas Wilfer <andreas@wilfer.se>
2017-04-17 13:23:37 +0200

    Removed lots of stuff

--- lisp/customized.el
+++ lisp/customized.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (lua-mode column-enforce-mode flycheck-cask flycheck-pos-tip flycheck-color-mode-line zenburn-theme zenburn zen-mode zen-and-art-theme use-package s request paredit nasm-mode markdown-mode langdoc helm flycheck epc company-quickhelp))))
+    (lua-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
