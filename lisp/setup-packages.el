be5cddea04be2db106918d1eabd87b3635b3c0f7
Andreas Wilfer <andreas@wilfer.se>
2018-01-04 11:47:06 +0100

    Added paredit hook to the scheme-mode

--- lisp/setup-packages.el
+++ lisp/setup-packages.el
@@ -49,6 +49,7 @@
   :config
   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
   (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode))
 
