8c675c5995274de659e2448e0c98a12e3631d292
Andreas Wilfer <andreas@wilfer.se>
2021-02-03 13:02:22 +0100

    Replace pushnew with cl-pushnew

--- lisp/errors.el
+++ lisp/errors.el
@@ -5,7 +5,7 @@
 (require 'compile)
 
 ;; ** cc65 and ca65 errors **
-(pushnew '("^\\([^ \n(]+\\)(\\([0-9]+\\)): \\(?:Error\\|Warnin\\(g\\)\\):" 1 2 nil (3))
+(cl-pushnew '("^\\([^ \n(]+\\)(\\([0-9]+\\)): \\(?:Error\\|Warnin\\(g\\)\\):" 1 2 nil (3))
 	 compilation-error-regexp-alist)
 
 ;;; errors.el ends here
