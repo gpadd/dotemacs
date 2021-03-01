802ce1ad4b9e603382168caccc8a4d0a9f4055ac
Andreas Wilfer <pawilfer@protonmail.com>
2021-03-01 12:57:36 +0100

    Customizing

--- lisp/customized.el
+++ lisp/customized.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(racket-mode clang-format geiser skewer-mode haskell-mode hydra ivy rust-mode erlang ruby-test-mode inf-ruby seeing-is-believing rbenv lua-mode column-enforce-mode flycheck-cask flycheck-pos-tip flycheck-color-mode-line zenburn-theme zenburn zen-mode zen-and-art-theme use-package s request paredit nasm-mode markdown-mode langdoc helm flycheck epc company-quickhelp)))
+   '(lsp-ivy company-box projectile lsp-mode racket-mode clang-format geiser skewer-mode haskell-mode hydra ivy rust-mode erlang ruby-test-mode inf-ruby seeing-is-believing rbenv lua-mode column-enforce-mode flycheck-cask flycheck-pos-tip flycheck-color-mode-line zenburn-theme zenburn zen-mode zen-and-art-theme use-package s request paredit nasm-mode markdown-mode langdoc helm flycheck epc company-quickhelp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
