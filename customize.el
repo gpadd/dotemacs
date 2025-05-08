7f58a81626cceeb7618a1c9e7fe920e4aec88dd8
Andreas Wilfer <andreas@wilfer.se>
2025-05-08 12:21:35 +0200

    Adding zenburn as a safe package

--- customize.el
+++ customize.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93" default))
  '(package-selected-packages
-   '(forth-mode slime uxntal-mode uxn-mode evil-tutor evil-collection evil paredit editorconfig company magit)))
+   '(zenburn-theme forth-mode slime uxntal-mode uxn-mode evil-tutor evil-collection evil paredit editorconfig company magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
