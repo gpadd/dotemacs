d392fed2eedbcdf7b685321b86de2ba619c5a413
Andreas Wilfer <andreas@wilfer.se>
2025-10-07 12:29:16 +0200

    Remove unused code and packages

--- customize.el
+++ customize.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93" default))
+   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93"
+     default))
  '(package-selected-packages
-   '(zenburn-theme slime evil-tutor evil-collection evil paredit editorconfig company magit)))
+   '(company editorconfig evil evil-collection evil-tutor magit
+	     paredit zenburn-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
