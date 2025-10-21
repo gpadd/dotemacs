8347b38c86815478226ddbea8a94ea8718ea2ea7
Andreas Wilfer <andreas@wilfer.se>
2025-10-21 12:00:11 +0200

    Add new packages and a theme
    Most in favor of lisp programming

--- customize.el
+++ customize.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company evil evil-collection evil-tutor magit paredit)))
+ '(custom-safe-themes
+   '("01be51d3a575f5f565aca6485b135e39ad5662d071326bc38855154fd062fc32"
+     default))
+ '(package-selected-packages
+   '(company evil evil-collection evil-tutor magit modus-themes nov
+	     paredit rainbow-delimiters slime)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
