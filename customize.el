8c1eaa544b62ad93a213966f2bcc495602c68daf
Andreas Wilfer <andreas@wilfer.se>
2026-02-09 14:08:31 +0100

    Remove nov as selected package

--- customize.el
+++ customize.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company evil-collection evil-tutor magit nov paredit
+   '(company evil-collection evil-tutor magit paredit
 	     rainbow-delimiters slime)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
