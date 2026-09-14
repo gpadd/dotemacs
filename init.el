3eaf7229e37edeac41de9b0c797b7de1cab34338
Andreas Wilfer <andreas@wilfer.se>
2026-09-14 12:29:39 +0200

    Commenting out slime/sbcl
    Until I find a comfy replacement for sbcl

--- init.el
+++ init.el
@@ -55,12 +55,12 @@
   :load-path "lisp/"
   :commands forth-mode)
 
-(use-package slime
-  :ensure t
-  :config
-  (setq inferior-lisp-program "sbcl")
-  (slime-setup '(slime-fancy))
-  (global-set-key "\C-cs" 'slime-selector))
+;; (use-package slime
+;;   :ensure t
+;;   :config
+;;   (setq inferior-lisp-program "ecl")
+;;   (slime-setup '(slime-fancy))
+;;   (global-set-key "\C-cs" 'slime-selector))
 
 (use-package org-id)
 
